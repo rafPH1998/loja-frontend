cb758347471fee1ac9b49bf3ca6e60a2042b01ce
rafPH1998 <rafaelb89.rb@gmail.com>
2025-10-21 16:01:59 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Loja API",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
